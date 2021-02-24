3cda51b495c83c8f8d680bbd48804ea46ff3e128
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-02-24 15:10:24 +0200

    Add files via upload

---
+++ wwwroot/js/task03.js
@@ -0,0 +1,26 @@
+﻿function getRandomColor() {
+
+    var colors = ["blue", "green", "red", "yellow", "pink", "orange"];
+
+    colors.sort(function (a, b) { return 0.5 - Math.random() });
+    document.getElementById("cell1").style.backgroundColor = colors[0];
+    
+}
+
+function getRandomColor1() {
+
+    var colors = ["blue", "green", "red", "yellow", "pink", "orange"];
+
+    colors.sort(function (a, b) { return 0.5 - Math.random() });
+    document.getElementById("cell2").style.backgroundColor = colors[0];
+
+}
+
+function getRandomColor2() {
+
+    var colors = ["blue", "green", "red", "yellow", "pink", "orange"];
+
+    colors.sort(function (a, b) { return 0.5 - Math.random() });
+    document.getElementById("cell3").style.backgroundColor = colors[0];
+
+}
