b2be8cefd812b3e027a225a83e9a8fc5ef42b457
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-05-08 19:58:33 +0300

    Add files via upload

---
+++ Web Applications/Assignment/Migrations/20210505141636_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhotoSite.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    File = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Comment = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+        }
+    }
+}
---
+++ Web Applications/Assignment/Models/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoSite.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Upload)]
+        [Display(Name = "Upload photo")]
+        [Required(ErrorMessage = "Please choose photo to upload")]
+        public string File { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        
+        public string Comment { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date edited")]
+        public DateTime Date { get; set; }
+    }
+}
